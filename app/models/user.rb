b14c9c4830a093ea50b8b29d9711c17f71216125
VO VAN TRA <vovantra1198@EMAIL.com>
2018-12-16 22:24:32 +0600

    show data in table admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  	mount_uploader :image, AvatarUploader
 	has_secure_password
 	validates :email, presence: true
 	validates :password, presence: true
